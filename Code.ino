cbc76ebef80671e5333dbdfc3dc56b9f396e5385
UFelipeJR <jimenezramirezfelipe4@gmail.com>
2023-11-24 20:47:50 -0500

    line following

--- Code.ino
+++ Code.ino
@@ -1,40 +1,25 @@
 #include <QTRSensors.h>
 #include <EEPROMex.h>
+#include <Servo.h>
 
-//EEPROM Addressing for calibration storage
+// EEPROM Addressing for calibration storage
 #define addrCalibratedMinimumOn 0
 #define addrCalibratedMaximumOn 100
 
-#define blanco true
-#define negro false
+#define lineUmbral 500
 
-const int velBaseIni = 200;
+const int velBaseIni = 250;
 const int velBaseMax = 255;
-
 
 float kP = 0.05;
 float kD = 0.005;
 float kI = 0.0000955;
 
-
-// sensors 0 through 7 are connected to digital pins 3 through 10, respectively
 QTRSensors qtr;
 
 const uint8_t SensorCount = 8;
 unsigned int sensorValues[SensorCount];
 bool sensor[SensorCount];
-
-bool j2value;
-bool j1value;
-
-byte programa = 0;
-
-unsigned long previousMillis = 0;
-
-int cuentaInter = 0;
-int resetCuentaInter = 0;
-int maxResetCuentaInter = 4;
-int bloqueaLectura = 0;
 
 int P = 0;
 long I = 0;
@@ -54,7 +39,22 @@ int PWMB = 11;
 int BIN1 = 13; 
 int BIN2 = 12;
 
-void setup(){
+int pinSensor = A0;
+int pinBuzzer = A4;
+int val = 0;
+
+bool left = false;
+bool right = true;
+
+int wait = 450;
+int waitL = 3000;
+
+int minValue = 600;
+int maxValue = 900;
+
+Servo servo;
+
+void setup() {
   Serial.begin(9600);
   pinMode(STBY, OUTPUT);
   pinMode(PWMA, OUTPUT);
@@ -63,79 +63,105 @@ void setup(){
   pinMode(PWMB, OUTPUT);
   pinMode(BIN1, OUTPUT);
   pinMode(BIN2, OUTPUT);
+  servo.attach(A5);  
+  servo.write(15);
   qtr.setTypeRC();
-  qtr.setSensorPins((const uint8_t[]){A3,A2,A1,6,5,4,3,2}, SensorCount);
-  qtr.setEmitterPin(1);
+  qtr.setSensorPins((const uint8_t[]){A3, A2, A1, 6, 5, 4, 3, 2}, SensorCount);
   recallQTR();
 }
 
-void loop(){
+void loop() {  
   readSensors();
-
+  val = analogRead(pinSensor);
+  Serial.println(val);
   /*
   leerSerial();
   imprimirValores();
   return;
   */
- 
+  
+  //Motor 1 derecho
+  //Motor 2 izquierdo
+
   programSpeed(velBaseIni, velBaseMax);
   drive(velIzq, velDer); 
+
+  if(lineColor(0)&&lineColor(7)){
+    move(1,0,1);
+    move(2,0,1);
+    descarga();
+    delay(waitL);
+    if(right){
+      right = false;
+      left = true;
+      move(1,velBaseMax/2,2);
+      move(2,velBaseMax,1);
+      delay(wait);
+      move(1,0,1);
+      move(2,0,1);
+      delay(50);
+      move(1,velBaseMax,1);
+      move(2,velBaseMax,1);
+      delay(wait);
+    }
+    else{
+      move(1,velBaseMax,1);
+      move(2,velBaseMax/2,2);
+      descarga();
+      delay(wait-100);
+      move(1,0,1);
+      move(2,0,1);
+      delay(50);
+      move(1,velBaseMax,1);
+      move(2,velBaseMax,1);
+      delay(wait-100);
+      right = true;
+      left = false;
+    }
+    
+  }
+
+
+ 
 }
 
-
-
-
-
-
-void calibrateQTR()
-{
-  //drive(0, 0);
-
+void calibrateQTR() {
   delay(2000);
-  for (int i = 0; i < SensorCount; i++)
-  {
+
+  for (int i = 0; i < SensorCount; i++) {
     qtr.calibrationOn.minimum[i] = 1000;
     qtr.calibrationOn.maximum[i] = 0;
-
   }
 
   Serial.println();
   Serial.println("Beginning Calibration Process...");
 
-  for (int i = 0; i < 200; i++)  // make the calibration take about 10 seconds
-  {
-    qtr.calibrate();       // reads all sensors 10 times at 2500 us per read (i.e. ~25 ms per call)
+  for (int i = 0; i < 200; i++) {
+    qtr.calibrate();
   }
 
   Serial.println("Calibration Complete");
-
 }
-//**************************************************************************************************************
-void readQTR()
-{
+
+void readQTR() {
   Serial.println();
   Serial.println("Reading Calibration Data...");
 
-  for (int i = 0; i < SensorCount; i++)
-  {
+  for (int i = 0; i < SensorCount; i++) {
     Serial.print(qtr.calibrationOn.minimum[i]);
     Serial.print(' ');
   }
   Serial.println();
 
-  // print the calibration maximum values measured when emitters were on
-  for (int i = 0; i < SensorCount; i++)
-  {
+  for (int i = 0; i < SensorCount; i++) {
     Serial.print(qtr.calibrationOn.maximum[i]);
     Serial.print(' ');
   }
 
   delay(3000);
-
 }
-//**************************************************************************************************************
-void storeQTR()
-{
+
+void storeQTR() {
   Serial.println();
   Serial.println("Storing Calibration Data into EEPROM...");
 
@@ -144,9 +170,8 @@ void storeQTR()
 
   Serial.println("EEPROM Storage Complete");
 }
-//**************************************************************************************************************
-void recallQTR()
-{
+
+void recallQTR() {
   Serial.println();
   Serial.println("Recalling Calibration Data from EEPROM...");
 
@@ -156,30 +181,29 @@ void recallQTR()
 
   Serial.println("EEPROM Recall Complete");
 }
-//**************************************************************************************************************
-void leerSerial() {
 
-  if (Serial.available())
-  {
+void leerSerial() {
+  if (Serial.available()) {
     char ch = Serial.read();
 
-    if (ch == 'c' || ch == 'C')
-    {
+    if (ch == 'c' || ch == 'C') {
       calibrateQTR();
     }
-    else if (ch == 'r' || ch == 'R')
-    {
+    else if (ch == 'r' || ch == 'R') {
       readQTR();
     }
-    else if (ch == 's' || ch == 'S')
-    {
+    else if (ch == 's' || ch == 'S') {
       storeQTR();
     }
-    else if (ch == 'e' || ch == 'E')
-    {
+    else if (ch == 'e' || ch == 'E') {
       recallQTR();
     }
   }
+}
+
+void readSensors() {
+  unsigned int position = qtr.readLineBlack(sensorValues);
+  P = (position - 3500);
 
 }
 
@@ -196,86 +220,84 @@ void imprimirValores()
 
 }
 
-void readSensors() {
-  unsigned int position = qtr.readLineBlack(sensorValues);
-
-  P = (position - 3500);
-
-}
-
-void move(int motor, int speed, int direction){
-
-  digitalWrite(STBY, HIGH); 
+void move(int motor, int speed, int direction) {
+  digitalWrite(STBY, HIGH);
   boolean inPin1 = LOW;
   boolean inPin2 = HIGH;
 
- 
-
-  if(direction == 1){
+  if (direction == 1) {
     inPin1 = HIGH;
     inPin2 = LOW;
   }
 
-
-  if(motor == 1){
+  if (motor == 1) {
     digitalWrite(AIN1, inPin1);
     digitalWrite(AIN2, inPin2);
     analogWrite(PWMA, speed);
   }
-  else{
+  else {
     digitalWrite(BIN1, inPin1);
     digitalWrite(BIN2, inPin2);
     analogWrite(PWMB, speed);
   }
-
 }
 
-void stop(){
+void stop() {
   digitalWrite(STBY, LOW);
 }
 
-void drive(int speedl, int speedr)
-{
-  if (speedl > 0)
-  {
-    move(1,speedl,1);
-
+void drive(int speedl, int speedr) {
+  if (speedl > 0) {
+    move(1, speedl, 1);
   } else {
-    move(1,speedl*-1,2);
+    move(1, speedl * -1, 2);
   }
-  if (speedr > 0)
-  {
-    move(2,speedr,1);
+  if (speedr > 0) {
+    move(2, speedr, 1);
   } else {
-    move(2,speedr*-1,2);
+    move(2, speedr * -1, 2);
   }
 }
 
-void programSpeed(int velIni, int velMax)
-{
-
-  D = (P - LAST); /// ERROR MENOS EL ERROR ANTERIOR , DERIVATIVO
-  I = (I + P); //INTEGRAL
-
+void programSpeed(int velIni, int velMax) {
+  D = (P - LAST);
+  I = (I + P);
   if (I * P < 0) I = 0;
-
-  vel = (P * kP) + (D * kD) + (I * kI); // para velocidad 120//////estaba en 0.0925
-
+  vel = (P * kP) + (D * kD) + (I * kI);
   velIzq = velIni - vel;
   velDer = velIni + vel;
-
   if (velIzq > velMax) {
     velIzq = velMax;
-  } else if (velIzq < -velMax) {
-    velIzq = -velMax;
+  } else if (velIzq < -velMax) {    velIzq = -velMax;
   }
-
   if (velDer > velMax) {
     velDer = velMax;
   } else if (velDer < -velMax) {
     velDer = -velMax;
   }
-
   LAST = P;
+}
 
+
+bool lineColor(int numSensor){
+  if(sensorValues[numSensor] >= lineUmbral){
+    return true;
+  }
+  else{
+    return false;
+  }
+
+}
+
+void descarga(){
+  if(val > minValue && val < maxValue){
+    servo.write(90); 
+    delay(3000);
+    servo.write(15);
+    delay(5);
+  }
+  else{
+    servo.write(15); 
+  }
+  
 }
